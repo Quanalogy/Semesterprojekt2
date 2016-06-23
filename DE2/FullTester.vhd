3417fb50e58aaa63929534cc6aca2c95101d1ffd
agyring <assi90@gmail.com>
2016-06-23 13:43:29 +0200

    Ændret clocken på DE2 boardet til at køre på baud clocken

--- DE2/FullTester.vhd
+++ DE2/FullTester.vhd
@@ -20,7 +20,7 @@ signal indata								: std_logic_vector(16 downto 1) := (others => '0');
 begin
 gen			: entity work.BaudRateGenerator		port map (	clk => CLOCK_50, reset => KEY(0), clk_baud => clk_57600);
 rec			: entity work.Receiver				port map (	rxd => GPIO_0(0), reset => KEY(0), clk_baud => clk_57600, rxdata => indata, rxvalid => recvalid);
-codelock	: entity work.Code_Lock				port map (	clk => CLOCK_50, reset => KEY(0), codeEntry => recvalid, code => indata, lock => GPIO_1(0), clk_out=>GPIO_1(35));
+codelock	: entity work.Code_Lock				port map (	clk => clk_57600, reset => KEY(0), codeEntry => recvalid, code => indata, lock => GPIO_1(0), clk_out=>GPIO_1(35));
 GPIO_1(1) <= clk_57600;
 --LEDG <= indata(8 downto 1);
 LEDR(15 downto 0) <= indata(16 downto 1);
